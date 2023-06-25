af7940fc0d858166e71d691cea2898110012419c
taro-28 <tarotaro0208@gmail.com>
2023-06-25 17:22:00 +0900

    change todo to async

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,28 +1,18 @@
+"use client";
 import { Table } from "@/components/Table";
+import { useTodo } from "./useTodo";
 
 export default function Todo() {
+    const { todo } = useTodo();
     return (
-        <div>
-            <h1>Todo</h1>
+        <div className="space-y-4">
+            <h1 className="text-xl">Todo</h1>
             <Table
                 header={{
                     title: "Title",
                     feature: "Feature",
                 }}
-                items={[
-                    {
-                        title: "resizable",
-                        feature: "sidebar",
-                    },
-                    {
-                        title: "styling",
-                        feature: "table",
-                    },
-                    {
-                        title: "sort",
-                        feature: "table",
-                    },
-                ]}
+                items={todo}
             />
         </div>
     );
