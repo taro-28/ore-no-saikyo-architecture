d099c73eac16d70ee42a1c479faebe0482382ef7
taro-28 <tarotaro0208@gmail.com>
2023-06-25 17:21:39 +0900

    add loading comp

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { Icon } from "@iconify/react";
+
+export default function Loading() {
+    return (
+        <Icon icon="line-md:loading-loop" className="text-8xl text-gray-500" />
+    );
+}
